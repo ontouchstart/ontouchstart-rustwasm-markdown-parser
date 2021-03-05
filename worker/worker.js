9e77b0059075fa7355537ee34c4bb3b1cb3bd333
Sam Liu <ontouchstart@gmail.com>
2021-03-05 09:12:25 -0500

    init

---
+++ worker/worker.js
@@ -0,0 +1,17 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+/**
+ * Fetch and log a request
+ * @param {Request} request
+ */
+async function handleRequest(request) {
+  const { parse } = wasm_bindgen
+  await wasm_bindgen(wasm)
+  let markdown_input = "Hello world, this is a ~~complicated~~ *very simple* example.";
+  const output = parse(markdown_input)
+  let res = new Response(output, { status: 200 })
+  res.headers.set('Content-type', 'text/html')
+  return res
+}
